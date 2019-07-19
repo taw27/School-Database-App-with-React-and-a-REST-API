86bd92486b56f1e5e28f3eedd289abb719355232
TawshifTamjidAlam <tawshif27@gmail.com>
2019-07-18 18:30:49 -0700

    define User and Course models

---
+++ models/course.js
@@ -0,0 +1,23 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Course = sequelize.define(
+    "Course",
+    {
+      title: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      estimatedTime: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      materialsNeeded: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    },
+    {}
+  );
+  Course.associate = function(models) {
+    // associations can be defined here
+  };
+  return Course;
+};
