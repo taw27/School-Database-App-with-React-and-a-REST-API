b5c8acc241a8d1a672b78da2e025faae16ade0e4
TawshifTamjidAlam <tawshif27@gmail.com>
2019-07-25 17:36:10 -0700

    add courses router

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const usersRouter = require("./users");
+const coursesRouter = require("./courses");
 
 router.use("/users", usersRouter);
+router.use("/courses", ucoursesRouter);
 
 module.exports = router;
