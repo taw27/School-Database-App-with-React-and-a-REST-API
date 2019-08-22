0b1eaa9a01eb242a9520b527985341a0820fbac8
TawshifTamjidAlam <tawshif27@gmail.com>
2019-08-22 16:29:46 -0700

    add CourseItem component

---
+++ client/src/components/CourseItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const CourseItem = ({ id, title }) => {
+  return (
+    <div className="grid-33" to={`/courses/${id}`}>
+      <Link className="course--module course--link">
+        <h4 className="course--label">Course</h4>
+        <h3 className="course--title"> {title}</h3>
+      </Link>
+    </div>
+  );
+};
+
+CourseItem.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string
+};
+
+export default CourseItem;
