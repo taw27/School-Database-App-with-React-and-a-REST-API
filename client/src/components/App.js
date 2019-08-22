24c679bb50688e41ca70a576216df61a895f6f3c
TawshifTamjidAlam <tawshif27@gmail.com>
2019-08-22 16:58:38 -0700

    add courses components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Courses from "./Courses";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Courses />
     </div>
   );
 };
