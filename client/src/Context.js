1c4520b8158e8603801a87b1e5ec3aaa9792b135
TawshifTamjidAlam <tawshif27@gmail.com>
2019-08-21 18:43:03 -0700

    add value to Provider

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,10 +1,44 @@
-import { React, createContext, useContext } from "react";
+import { React, createContext, useContext, useState } from "react";
+import Data from "./Data";
+import Cookies from "js-cookie";
 import PropTypes from "prop-types";
 
 export const context = createContext();
 
 export const Provider = ({ children }) => {
-  return <context.Provider>{children}</context.Provider>;
+  const [authenticatedUser, setAuthenticatedUser] = useState(
+    Cookies.getJSON("authenticatedUser") || null
+  );
+  const data = new data();
+
+  const signIn = async (email, passWord) => {
+    const user = await data.getUser(email, passWord);
+    if (user !== null) {
+      setAuthenticatedUser({ ...user, passWord });
+      const cookieOptions = {
+        expires: 1 // 1 day
+      };
+      // TODO: change to token based authentication later
+      Cookies.set("authenticatedUser", JSON.stringify(user), { cookieOptions });
+    }
+
+    return user;
+  };
+
+  const signOut = () => {
+    Cookies.remove("authenticatedUser");
+    setAuthenticatedUser(null);
+  };
+
+  const value = {
+    authenticatedUser,
+    data,
+    actions: {
+      signIn,
+      signOut
+    }
+  };
+  return <context.Provider value={value}>{children}</context.Provider>;
 };
 
 const useContextValue = () => useContext(context);
