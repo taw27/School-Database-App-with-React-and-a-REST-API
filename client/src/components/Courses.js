eb4879fd4b3f5848360d42496f86919f2af96db7
TawshifTamjidAlam <tawshif27@gmail.com>
2019-08-22 16:58:30 -0700

    add render with course items

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react";
+import CourseItem from "./CourseItem";
 import useContextValue from "../Context";
 
 const Courses = () => {
   const [courses, setCourses] = useState([]);
-  const { data } = useContextValue;
+  const { data } = useContextValue();
 
   useEffect(() => {
     data
       .getCourses()
-      .then(courses => setCourses(courses))
-      .catch(() => setCourses(null));
+      .then(courses => {
+        setCourses(courses);
+      })
+      .catch(() => setCourses([]));
   }, []);
 
-  return {};
+  return (
+    <div className="bounds">
+      {courses.map(course => (
+        <CourseItem id={course.id} key={course.id} title={course.title} />
+      ))}
+    </div>
+  );
 };
 
 export default Courses;
