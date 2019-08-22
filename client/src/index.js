203905dc8e514b8e0a9031537404fbaa47714ad4
TawshifTamjidAlam <tawshif27@gmail.com>
2019-08-21 19:17:13 -0700

    add react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from "./Context";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
