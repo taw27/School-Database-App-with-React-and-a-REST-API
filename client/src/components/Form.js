f8109bccf2d0caed74bbef2e7e733419251c7336
TawshifTamjidAlam <tawshif27@gmail.com>
2019-08-24 16:12:41 -0700

    create from component, erro display remaining

---
+++ client/src/components/Form.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Form = ({
+  title,
+  submitButtonText,
+  formElements,
+  cancel,
+  submit,
+  errors,
+  alternateOption
+}) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    submit();
+  };
+
+  const handleCancel = e => {
+    e.preventDefault();
+    cancel();
+  };
+
+  return (
+    <div className="grid-33 centered signin">
+      <h1>{title}</h1>
+      <div>
+        <form>
+          {formElements()}
+          <div className="grid-100 pad-bottom">
+            <button className="button" type="submit" onClick={handleSubmit}>
+              {submitButtonText}
+            </button>
+            <button className="button button-secondary" onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+      <p>&nbsp;</p>
+      {alternateOption()}
+    </div>
+  );
+};
+
+Form.propTypes = {
+  title: PropTypes.string,
+  submitButtonText: PropTypes.string,
+  alternateOption: PropTypes.func,
+  formElements: PropTypes.func,
+  cancel: PropTypes.func,
+  submit: PropTypes.func,
+  errors: PropTypes.array
+};
+
+export default Form;
