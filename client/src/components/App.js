29b0e448d03bc1a2857619b47e0de977cf4e68c2
TawshifTamjidAlam <tawshif27@gmail.com>
2019-08-24 23:47:04 -0700

    add route handling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Header from "./Header";
 import Courses from "./Courses";
+import UserSignIn from "./UserSignIn.js";
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Header />
       <div className="bounds">
-        <Courses />
+        <Switch>
+          <Route exact path="/" component={Courses} />
+          <Route exact path="/signin" component={UserSignIn} />
+        </Switch>
       </div>
     </div>
   );
